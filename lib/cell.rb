a6b9ee71dc50c5428db1794450a5e28b2e948a9b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-02 17:28:20 -0600

    Work on render method in cell file and cell test file
    Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -26,8 +26,21 @@ class Cell
   end
 
   def fire_upon
-    @ship.hit
+    @fired_upon = true
+    if empty? == false
+     return @ship.hit
+    end
   end
 
-
-end
+  def render
+    # if @ship.health === 0
+    #   return "X"
+    # if @ship.health < 3 && @ship.nil? == false
+    #   return "H"
+    if empty? && @fired_upon == true
+      return "M"
+    else empty? && @fired_upon == false
+      return "."
+    end
+    end
+  end
