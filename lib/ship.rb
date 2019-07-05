d95a412c47abf7b80a0d6b71b2a2497154a52419
Scott Payton <spayton.c@gmail.com>
2019-07-05 12:08:37 -0600

    Updated Ship file to Head and added Functionality

---
+++ lib/ship.rb
@@ -0,0 +1,18 @@
+class Ship
+  attr_reader :name, :health, :length
+  def initialize(name, health)
+    @name = name
+    @health = health
+    @length = health
+  end
+
+  def sunk?
+    @health === 0 #evaluates to true if 0
+  end
+
+  def hit
+    @health -= 1  #need a space between the - 1 when doing this.
+    #@health
+  end
+
+end #end class
