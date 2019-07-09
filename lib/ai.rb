76073d9838e61dc24ce7245feed53fadd9c7dd69
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-08 18:33:24 -0600

    Add place ship method to AI file and AI test file
    Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,23 +1,51 @@
 class Ai
+  attr_reader :ai_board
   #TODO
 ##ai will be born with their own board and a userboard to fire upon "targets"
 ##ai will be born with 2 ships
 ##ai must reference a random pool of targets, coordinate hash
 ##methods: fire(targets?), method to update new targets after firing, place ships, #validate sampled coordinates
   def initialize(ai_board, user_board)
-    @ai_board = ai_board
-    @user_board = user_board
-    @targets = user_board.cells.keys.shuffle #randmizes potential targets for ai?
-    @ai_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine",2 )]
+    @ai_board = Board.new
+    @user_board = Board.new
+    @targets = @user_board.cells.keys.shuffle #randmizes potential targets for ai?
+    @ai_ship_cruiser = Ship.new("Cruiser", 3)
+    @ai_ship_submarine = Ship.new("Submarine", 2)
   end
 
-  def validate_sampled_coordinates
-    #TODO: should access sampled coordinate values from hash keys
-    #and validate they match the ai_ships? 
-  end
+  def place_cruiser_ai(board)
+    (@ai_board.cells.length * 2).times do
+      coordinate_array_1 = []
+      coordinate_array_1 << @ai_board.cells.keys.sample
+      letter_value = coordinate_array_1.first[0].ord
+      letter_value_2 = letter_value +1
+      letter_value_3 = letter_value +2
 
-  def place_ship_ai
-    #TODO: accesses computer ships, and for each one
+
+      coordinate_array_1 << letter_value_2.chr + coordinate_array_1.first[1]
+
+      coordinate_array_1 << letter_value_3.chr + coordinate_array_1.first[1]
+
+      if @ai_board.valid_placement?(@ai_ship_cruiser, coordinate_array_1)
+
+        return board.place(@ai_ship_cruiser, coordinate_array_1)
+        # require 'pry'; binding.pry
+      end
+
+      coordinate_array_1.pop(2)
+      number_value = coordinate_array_1.first[1].to_i
+      number_value_2 = number_value +1
+      number_value_3 = number_value +2
+      coordinate_array_1 << coordinate_array_1.first[0] + number_value_2.to_s
+      coordinate_array_1 << coordinate_array_1.first[0] + number_value_3.to_s
+
+      if @ai_board.valid_placement?(@ai_ship_cruiser, coordinate_array_1)
+        @ai_board.place(@ai_ship_cruiser, coordinate_array_1)
+      end
+    end
+
+
+
     #validates a random set of coordinates and calls place ship method
   end
 
--- test/ai_test.rb
+++ test/ai_test.rb
@@ -11,6 +11,8 @@ class AiTest < MiniTest::Test
     @ai_board = Board.new
     @user_board = Board.new
     @ai = Ai.new(@ai_board, @user_board)
+    @submarine = Ship.new("Submarine", 2)
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   #TODO:
@@ -18,24 +20,40 @@ class AiTest < MiniTest::Test
 #tests for validation of existence,
 
   def test_ai_exists
+    skip
 
   end
+
+  def test_ai_has_a_board_rendered
+
+    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", @ai_board.render
+  end
 #has 2 ships by defualt,
+  def test_ai_has_1_ship
+    @ai.place_cruiser_ai(@ai_board)
+
+    assert_equal "  1 2 3 4 \nA S . S S \nB . . . . \nC . . . . \nD . . . . \n", @ai_board.render(true)
+  end
+
   def test_ai_has_2_ships
+    skip
 
   end
 
 #has access to hash for targets,
   def test_it_has_hash_count_of_targets_to_fire_upon
+    skip
 
   end
 
 #test it can fire 16 times
   def test_it_can_fire_at_the_hash_coordinate_count_times
+    skip
 
   end
 #test it can place valid ships randomly
   def test_it_can_validate_sample_coords_for_ship_placement
+    skip
   end
 
-end #end test 
+end #end test
