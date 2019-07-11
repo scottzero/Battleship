0c74287fb175da91d70c68e81d1b2c830de51263
Scott Payton <spayton.c@gmail.com>
2019-07-10 19:46:59 -0600

    Finalized ai, Clean and working

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,36 +1,15 @@
-require 'pry'
 class Ai
   attr_reader :ai_board, :possible_targets, :ai_ship_cruiser, :ai_ship_submarine
-  #TODO
-##ai will be born with their own board and a userboard to fire upon "targets"
-##ai will be born with 2 ships
-##ai must reference a random pool of targets, coordinate hash
-##methods: fire(targets?), method to update new targets after firing, place ships, #validate sampled coordinates
   def initialize(ai_board, user_board)
     @ai_board = Board.new
     @user_board = Board.new
-    @possible_targets = @user_board.cells.keys.shuffle #randmizes potential targets for ai?
+    @possible_targets = @user_board.cells.keys.shuffle
     @ai_ship_cruiser = Ship.new("Cruiser", 3)
     @ai_ship_submarine = Ship.new("Submarine", 2)
   end
 
   def fire(board, target)
-
       board.cells[target].fire_upon
-      # require 'pry'; binding.pry
-      # coordinate_array = user_board.cells.keys
-      # coordinate_array.flatten
-      # fired_coordinates_array = []
-      #
-      #
-      # fired_coordinates_array << coordinate_array.delete(coordinate_array.sample)
-      # # require 'pry'; binding.pry
-      #
-      # if user_board.cells[fired_coordinates_array.last].fired_upon == false
-      #
-      #   user_board.cells[fired_coordinates_array.last].fire_upon
-    # end
-
   end
 
   def target
@@ -44,36 +23,22 @@ class Ai
       letter_value = coordinate_array_1.first[0].ord
       letter_value_2 = letter_value +1
       letter_value_3 = letter_value +2
-
-
       coordinate_array_1 << letter_value_2.chr + coordinate_array_1.first[1]
-
       coordinate_array_1 << letter_value_3.chr + coordinate_array_1.first[1]
-
       if board.valid_placement?(@ai_ship_cruiser, coordinate_array_1)
-
         return board.place(@ai_ship_cruiser, coordinate_array_1)
-        # require 'pry'; binding.pry
       end
-
       coordinate_array_1.pop(2)
       number_value = coordinate_array_1.first[1].to_i
       number_value_2 = number_value +1
       number_value_3 = number_value +2
       coordinate_array_1 << coordinate_array_1.first[0] + number_value_2.to_s
       coordinate_array_1 << coordinate_array_1.first[0] + number_value_3.to_s
-
       if board.valid_placement?(@ai_ship_cruiser, coordinate_array_1)
         @ai_board.place(@ai_ship_cruiser, coordinate_array_1)
       end
-
     end
-
-
-
-
-    #validates a random set of coordinates and calls place ship method
-  end
+end
 
 
 
